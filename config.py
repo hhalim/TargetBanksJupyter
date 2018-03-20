29f45a21e8b6db8325bd40a1eb51f3399e162f8e
Hartono Halim <gopher@equalbyte.com>
2018-03-20 08:33:36 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Global Configuration
+"""
+
+mssql = {
+    'server' : 'localhost\SQL2016',
+    'database' : 'bkrob',
+    'username' : 'bkrob_adm',
+    'password' : 'bkrob_adm'
+}
+
+google_geocode_api = 'api key'
